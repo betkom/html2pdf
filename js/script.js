7b61e0903c4d8336cefe46b792fff8afba0ebee0
betkom <betkomuniq@gmail.com>
2020-05-27 12:52:16 -0400

    add margin

--- js/script.js
+++ js/script.js
@@ -1,39 +1,15 @@
 
-function generate(e) {
-  console.log('got called')
-  const domElement = document.getElementById('pricing-table')
-  html2canvas(domElement, { onclone: (document) => {
-    document.getElementById('print-button').style.visibility = 'hidden'
-  }})
-  .then((canvas) => {
-    const img = canvas.toDataURL('image/png')
-    const pdf = new jsPdf('p', 'pt', 'a4')
-    console.log('generating...')
-    pdf.addImage(img, 'JPEG', 0, 0, width, height)
-    pdf.save('tolu.pdf')
-  })
-}
-
-document.getElementById('generate').onclick = generate
-
-// generate = function()
-// {
-// 	var pdf = new jsPDF('p', 'pt', 'a4');
-// 	pdf.setFontSize(18);
-// 	pdf.fromHTML(document.getElementById('html-2-pdfwrapper'), 
-// 		margins.left, // x coord
-// 		margins.top,
-// 		{
-// 			// y coord
-// 			width: margins.width// max width of content on PDF
-// 		},function(dispose) {
-// 			headerFooterFormatting(pdf, pdf.internal.getNumberOfPages());
-// 		}, 
-// 		margins);
-		
-// 	var iframe = document.createElement('iframe');
-// 	iframe.setAttribute('style','position:absolute;right:0; top:0; bottom:0; height:100%; width:650px; padding:20px;');
-// 	document.body.appendChild(iframe);
+generate = function() {
 	
-// 	iframe.src = pdf.output('datauristring');
-// };
+	var pdf = new jsPDF('p', 'pt', 'a4', true);
+	pdf.setFontSize(14);
+
+	pdf.html(document.getElementById('pricing-table'),
+    {
+      x: 30,
+			html2canvas: {width: 550, scale: 0.6},
+			callback: function () {
+					pdf.save('document.pdf');
+			}
+    });
+};
